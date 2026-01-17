2189da1849adcf2bb3d87a814228b47c507cbd71
Илья Полкунов <ef1f@mail.ru>
2026-01-17 17:48:53 +0300

    Добавил комментарии

--- TestTask.Application/Interfaces/ITransactionService.cs
+++ TestTask.Application/Interfaces/ITransactionService.cs
@@ -1,13 +1,30 @@
-﻿
-using TestTask.Core;
+﻿using TestTask.Core;
 using TestTask.Core.Models;
 
 namespace TestTask.Application.Interfaces;
 
+/// <summary>
+/// Сервис выполнения транзакций
+/// </summary>
 public interface ITransactionService
 {
+    /// <summary>
+    /// Операция начисления
+    /// </summary>
     Task<TransactionResponse> CreditAsync(ITransaction transaction, CancellationToken token);
+
+    /// <summary>
+    /// Операция списания 
+    /// </summary>
     Task<TransactionResponse> DebitAsync(ITransaction transaction, CancellationToken token);
+
+    /// <summary>
+    /// Отмена транзакции
+    /// </summary>
     Task<RevertResponse> RevertAsync(Guid transactionId, CancellationToken token);
+
+    /// <summary>
+    /// Получение текущего баланса
+    /// </summary>
     Task<BalanceResponse> GetBalanceAsync(Guid clientId, CancellationToken token);
 }
--- TestTask.Application/Interfaces/ITransactionStrategy.cs
+++ TestTask.Application/Interfaces/ITransactionStrategy.cs
@@ -2,9 +2,23 @@
 
 namespace TestTask.Application.Interfaces;
 
+/// <summary>
+/// Стратегия обработки финансовой операции.
+/// </summary>
 public interface ITransactionStrategy
 {
+    /// <summary>
+    /// Тип транзакции
+    /// </summary>
     TransactionType Type { get; }
+
+    /// <summary>
+    /// Выполняет основную бизнес-логику изменения баланса клиента
+    /// </summary>
     void Apply(Client client, decimal amount);
+
+    /// <summary>
+    /// Выполняет отмену операции, (откат) баланса
+    /// </summary>
     void Revert(Client client, decimal amount);
 }
