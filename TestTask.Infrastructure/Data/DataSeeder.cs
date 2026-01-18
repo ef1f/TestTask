5147d9b87f390c6a9bd42ba758a649c84c1476af
Илья Полкунов <ef1f@mail.ru>
2026-01-18 13:00:21 +0300

    правка

---
+++ TestTask.Infrastructure/Data/DataSeeder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Core.Entities;
+
+namespace TestTask.Infrastructure.Data;
+
+public static class DataSeeder
+{
+    public static void SeedDatabase(ApplicationDbContext dbContext)
+    {
+        if (dbContext.Clients.Any())
+        {
+            return;
+        }
+
+        var client = new Client
+        {
+            Id = Guid.Parse("cfaa0d3f-7fea-4423-9f69-ebff826e2f89"),
+            Name = "Клиент 1",
+            Balance = 0m
+        };
+
+        dbContext.Clients.Add(client);
+        dbContext.SaveChanges();
+    }
+}
